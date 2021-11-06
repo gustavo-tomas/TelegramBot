f66f394acda3c571d0a6582c8db3cf32783bed74
Gustavo Tomás <guga.tomas10@gmail.com>
2021-11-06 20:53:24 -0300

    added poke command

--- commands.py
+++ commands.py
@@ -1,14 +1,28 @@
 def start(update, context):
-  return update.message.reply_text("How do u do fellow kids?")
+  return update.message.reply_text(
+    text="How do u do fellow kids?\nType `/help` to see available commands",
+    parse_mode='MarkdownV2')
 
 def help(update, context):
   message = """
   The following commands are available:
 
-  /start -> Welcome Message
-  /help -> Display Available commands
+  /start \-\> Welcome Message
+  /help \-\> Display available commands
+  /poke `chat_id` \-\> Poke user with specified id
   """
-  return update.message.reply_text(message)
+  return update.message.reply_text(message, parse_mode='MarkdownV2')
 
+def poke(update, context):
+  if len(context.args) <= 0 or context.args[0] != context.args[0]:
+    return update.message.reply_text('Please specify a valid id')
+  print("Poking " + context.args[0])
+  try:
+    context.bot.sendMessage(chat_id=context.args[0], text="Hewwo :)")
+  except:
+    update.message.reply_text('Failed to poke user ' + context.args[0])
+  finally:  
+    return 
+  
 def handle_message(update, context):
   return update.message.reply_text(f"Echo {update.message.text}")
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
   updater = telegram.ext.Updater(TOKEN, use_context=True)
   updater.dispatcher.add_handler(telegram.ext.CommandHandler("start", start))
   updater.dispatcher.add_handler(telegram.ext.CommandHandler("help", help))
+  updater.dispatcher.add_handler(telegram.ext.CommandHandler("poke", poke))
   updater.dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
 
   updater.start_polling()
@@ -15,4 +16,5 @@ def main():
   return
 
 if __name__ == "__main__":
+  print("TelegramBot is ready!")
   main()
